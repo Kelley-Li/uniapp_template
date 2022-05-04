9993d55c8031a29f5bbd0fd796b5ec56848e23e1
lzz <1447968723@qq.com>
2022-05-04 23:17:08 +0800

    '封装request'

--- utils/request/test.js
+++ utils/request/test.js
@@ -1,24 +1,34 @@
-export default class Request{
+export default class Request {
 	config: {
 		baseUrl: '',
 		header: {
 			'Content-Type': ''
 		},
-		data: {},
+		data: {}, // Object/String/ArrayBuffer	 请求的参数
 		method: "GET",
-		dataType: "json",
-		/* 如设为json，会对返回的数据做一次 JSON.parse */
-		responseType: "text",
-		timeout: 30000,
-		success() {},
-		fail() {},
-		complete() {}
+		dataType: "json", // 如设为json，会对返回的数据做一次 JSON.parse
+		responseType: "text", // 设置响应的数据类型。合法值：text、arraybuffer
+		timeout: 30000, // 超时时间，单位 ms
+		custom,
+		sslVerify: true, // 是否验证 ssl 证书
+		success() {}, // 收到开发者服务器成功返回的回调函数
+		fail() {}, // 接口调用失败的回调函数
+		complete() {} // 接口调用结束的回调函数（调用成功、失败都会执行）
 	}
-	
+
 	/* 判断url是否为绝对路径 */
 	static verifyUrl(url) {
 		return /(http|https):\/\/([\w.]+\/?)\S*/.test(url)
-		
+	}
+
+	/* 拼接请求参数 */
+	static addQueryParams(params) {
+		let paramsData = ''
+		Object.keys(params).forEach(key => {
+			// 把字符串作为 URI 组件进行编码。
+			paramsData += key + '=' + encodeURIComponent(params[key])
+		})
+		return paramsData.substring(0, paramsData.length - 1)
 	}
 
 	/* url路径拼接 */
@@ -31,28 +41,148 @@ export default class Request{
 		}
 	}
 
-	/* 拼接请求参数 */
-	static addQueryParams(params) {
-		let paramsData = ''
-		Object.keys(params).forEach(key => {
-			// 把字符串作为 URI 组件进行编码。
-			paramsData += key + '=' + encodeURIComponent(params[key])
-		})
-		return paramsData.substring(0, paramsData.length - 1)
-	}
+
 	/**
 	 * @property {Function} request 请求拦截器
 	 * @property {Function} response 响应拦截器
-	 * @type {{request: Request.interceptor.request, response: Request.interceptor.response}}
+	 * @type {{request: Request.interceptor.request, 
+			   response: Request.interceptor.response}}
 	 */
 	interceptor: {
-		
+
 		/**
-		 * @param {Request~requestCallback} cb - 请求之前拦截,接收一个函数（config, cancel）=> {return config}。第一个参数为全局config,第二个参数为函数，调用则取消本次请求。
+		 * @param {Request~requestCallback} cb - 请求之前拦截,
+		 * 接收一个函数（config, cancel）=> {return config}。
+		 * 第一个参数为全局config,第二个参数为函数，调用则取消本次请求。
 		 */
-		request: (callback)=>{
-			
+		request: (cb) => {
+			if (cb) {
+				this.requestBeforFun = cb
+			}
 		},
-		response: null
+		/**
+		 * @param {Request~responseCallback} cb 响应拦截器，对响应数据做点什么
+		 * @param {Request~responseErrCallback} ecb 响应拦截器，对响应错误做点什么
+		 */
+		response: (cb, ecb) {
+			if (ob) {
+				this.requestComFun = ob
+			}
+			if (ecb) {
+				this.requestComFail = ecb
+			}
+		}
+	}
+
+	/* 请求之前的函数 */
+	requestBeforFun(config) {
+		return config
+	}
+	/* 请求成功的函数 */
+	requestComFun(response) {
+		return response
+	}
+	/* 请求失败的函数 */
+	requestComFail(response) {
+		return response
+	}
+
+	/**
+	 * 自定义验证器，如果返回true 则进入响应拦截器的响应成功函数(resolve)，
+	 * 否则进入响应拦截器的响应错误函数(reject)
+	 * @param { Number } statusCode - 请求响应体statusCode（只读）
+	 * @return { Boolean } 如果为true,则 resolve, 否则 reject
+	 */
+	validateStatus(statusCode) {
+		return statusCode === 200
+	}
+
+	/**
+	 * @Function
+	 * @param {Request~setConfigCallback}f --> function 设置全局默认配置 
+	 */
+	setConfig(f) {
+		this.config = f(this.config)
+	}
+
+	/**
+	 * @Function 参数说明
+	 * @param {Object} options - 请求配置项
+	 * @prop {String} options.baseUrl - 请求地址
+	 * @prop {String} options.url - 请求路径
+	 * @prop {String} options.timeout - 请求超时时间
+	 * @prop {Object} [options.dataType = config.dataType] - 
+	 * 				如果设为 json，会尝试对返回的数据做一次 JSON.parse
+	 * @prop {Object} options.data - 请求参数
+	 * @prop {Object} [options.responseType = config.responseType] 
+	 * 				[text|arraybuffer] - 响应的数据类型
+	 * @prop {Object} [options.header = config.header] - 请求header
+	 * @prop {Object} [options.method = config.method] - 请求方法
+	 * @prop {Object} [options.sslVerify ||  config.method] - 是否验证 ssl 证书
+	 * @returns {Promise<unknown>}
+	 */
+
+	request(options = {}) {
+		options.baseUrl = options.baseUrl || this.config.baseUrl //请求地址
+		options.url = options.baseUrl + options.url //请求路径
+		options.timeout = options.timeout || this.config.timeout // 请求超时时间
+		options.dataType = options.dataType || this.config.dataType // 如果设为 json，会尝试对返回的数据做一次 JSON.parse
+		options.data = options.data || {} // Object/String/ArrayBuffer	否		请求的参数
+		options.params = options.params || {}
+		options.responseType = options.responseType || this.config.responseType // 设置响应的数据类型
+		options.header = options.header || this.config.header // 设置请求的 header，header 中不能设置 Referer。
+		options.method = options.method || this.config.method
+		options.custom = {
+			...this.config.custom,
+			...(options.custom || {})
+		}
+		// #ifdef APP-PLUS
+		options.sslVerify = options.sslVerify === undefined ? this.config.sslVerify : options.sslVerify
+		// #endif
+
+		return new Promise((resolve, reject) => {
+			let next = true
+			const cancel = (t = 'handle candel', config = options) => {
+				const err = {
+					errMsg: t,
+					config: config
+				}
+				reject(err)
+				next = false
+			}
+			const handel = {
+				...this.requestBeforFun(options, cancel)
+			}
+			const _config = {
+				...handel
+			}
+			if (!next) return
+			const requestTask = uni.request({
+				url: Request.mergeUrl(_config.url, _config.baseUrl, _config.params),
+				data: _config.data,
+				header: _config.header,
+				method: _config.method,
+				// #ifdef MP-ALIPAY
+				timeout: _config.timeout,
+				// #endif
+				dataType: _config.dataType,
+				// #ifndef MP-ALIPAY || APP-PLUS
+				responseType: _config.responseType,
+				// #endif
+				// #ifdef APP-PLUS
+				sslVerify: _config.sslVerify,
+				// #endif
+				complete: (response) => {
+					response.config = handleRe
+					if (this.validateStatus(response.statusCode)) { // 成功
+						response = this.requestComFun(response)
+						resolve(response)
+					} else {
+						response = this.requestComFail(response)
+						reject(response)
+					}
+				}
+			})
+		})
 	}
 }
